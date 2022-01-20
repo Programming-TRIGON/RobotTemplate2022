d826612bf9cd345271be0a77a1e8c22897c6ff84
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2022-01-20 09:59:24 +0200

    Updated Led Class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,26 +1,31 @@
 package frc.robot;
 
-
+import frc.robot.constants.FieldConstants;
+import frc.robot.constants.RobotConstants;
 import frc.robot.utilities.DashboardController;
 
-public class RobotContainer{
+public class RobotContainer {
     private final DashboardController dashboardController;
+    private final RobotConstants robotConstants;
+    private final FieldConstants fieldConstants;
 
     /**
      * Add classes here
      */
-    public RobotContainer(){
+    public RobotContainer() {
         dashboardController = new DashboardController();
+        robotConstants = new RobotConstants();
+        fieldConstants = new FieldConstants();
     }
 
     /**
      * initializes all commands
      */
-    public void initializeCommands(){
+    public void initializeCommands() {
 
     }
 
-    public void updateDashboard(){
+    public void updateDashboard() {
         dashboardController.update();
     }
 }
